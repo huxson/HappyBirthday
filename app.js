a5f44f1ec178936d8a69a93b65bb936860f688da
huxson <46333130+huxson@users.noreply.github.com>
2019-01-02 19:47:12 -0500

    append to box

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ var birthday = function() {
     count += 111
     console.log("HAPPY BIRTHDAY!!!")
     $("#counter").text(count)
+    $("#box").append(" Happy Birthday!!!")
 }
 setInterval(birthday, 1)
